a3a2d8b71b16cd9175473e566f8eb5ca372fe362
adilw <adilwankhede@live.com>
2023-05-24 13:10:49 +0200

    update-plot animation at different times, plot at selected time instant

--- python_scripts/input_data.py
+++ python_scripts/input_data.py
@@ -4,58 +4,60 @@ Created on Sat May 20 15:53:05 2023
 
 read nc file and crop data
 
-@author: Adil Wankhede
+@author: ADIMATE
 """
-import netCDF4
+# import netCDF4
 import numpy as np
 import xarray as xr
+# import pandas as pd
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import time
 
-f1 = netCDF4.Dataset('.//data/MetO-NWS-WAV-hi_1684785430307.nc')
-f2 = netCDF4.Dataset('.//data/GlobalOcean.nc')
-# print(f)
+# import cartopy.crs as ccrs
+# import time
 
-# print(f.variables.keys()) # get all variable names
 
-SWH = f1.variables['VHM0']  # Significant wave height variable
-VX = f1.variables['VSDX']  # wave stokes drift x velocity variable
-VY = f1.variables['VSDY']  # wave stokes drift y velocity variable
-print(SWH)
-
-# SWH.dimensions
-# SWH.shape
-
-tim = f1.variables['time']
-lat, long = f1.variables['latitude'], f1.variables['longitude']
-
-tm = tim[:]
-lt = lat[:]
-lg = long[:]
-
-# print(tm)
-
+# Reading nc files as xarray
 DS1 = xr.open_dataset(".//data/MetO-NWS-WAV-hi_1684785430307.nc")
 DS2 = xr.open_dataset(".//data/GlobalOcean.nc")
 print(DS1)
 
-plt.figure(1)
-DS1.VHM0.isel(time=720).plot()
-plt.show()
+# Plotting
+# for i in range(len(DS1.time)):
+#     if i%10==0:
+#         plt.figure(1)
+#         DS1.VHM0.isel(time=i).plot()
+#         plt.show()
+
+t = input("Enter date (yyyy-mm-dd):")
+h = input("Enter time 00-23 (hh):")
+tt = t + "T" + h + ":00:00"
+# tt="2023-04-01T23:30:00"
+# t=np.datetime64(tt)
+
 
 plt.figure(2)
-DS2.uo.isel(time=0).plot()
+DS2.utotal.sel(time=tt, method="nearest").plot()
 plt.show()
 
-# Plot a 2D map for 21st April 2023
+# f1 = netCDF4.Dataset('.//data/MetO-NWS-WAV-hi_1684785430307.nc')
+# f2 = netCDF4.Dataset('.//data/GlobalOcean.nc')
+# #print(f)
 
-# fig1=DS.VSDX.isel(time=500).plot()
-# fig2=DS.VSDY.isel(time=500).plot()
-# for i in range(len(tm)):
-#     plt.figure()
-#     DS.VHM0.isel(time=i).plot()
+# #print(f.variables.keys()) # get all variable names
 
-#     time.sleep(0.5) # Sleep for 0.5 seconds
+# SWH = f1.variables['VHM0'] # Significant wave height variable
+# VX = f1.variables['VSDX'] # wave stokes drift x velocity variable
+# VY = f1.variables['VSDY'] # wave stokes drift y velocity variable
+# print(SWH)
 
+# # SWH.dimensions
+# # SWH.shape
 
+# tim = f1.variables['time']
+# lat,long = f1.variables['latitude'], f1.variables['longitude']
+
+# tm=tim[:]
+# lt=lat[:]
+# lg=long[:]
+
+# print(tm)
